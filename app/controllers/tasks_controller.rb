bb8f3a39ce4ef5808c4ce15b7aebd8464ec56463
shinya <shinyagit@gmail.com>
2020-08-30 06:50:57 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,21 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
   end
   
   def show
   end
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:succes] = 'タスクが正常に登録されました'
       redirect_to @task
@@ -40,7 +40,6 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
@@ -48,7 +47,14 @@ class TasksController < ApplicationController
   private
 
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
   # Strong Parameter
