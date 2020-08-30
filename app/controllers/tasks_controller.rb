4891730e0b995be9f562250e65385daac859120e
shinya <shinyagit@gmail.com>
2020-08-30 10:58:53 +0000

    fix tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
@@ -45,10 +44,6 @@ class TasksController < ApplicationController
   end
   
   private
-
-  def set_task
-    @task = current_user.tasks.find(params[:id])
-  end
 
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
